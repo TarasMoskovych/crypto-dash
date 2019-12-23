1c64051aab2b0629550ffebacba4451ffc44624b
Moskovych_Taras <taras_moskovych@epam.com>
2019-12-23 14:49:26 +0200

    Updates

---
+++ precache-manifest.afb23081da42de895b81dcc58223cf23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5b609bd0bd1a8e864861116edac420",
+    "url": "/crypto-dash/index.html"
+  },
+  {
+    "revision": "7e9233214477ae1aa61f",
+    "url": "/crypto-dash/static/css/main.bf34527f.chunk.css"
+  },
+  {
+    "revision": "33165f111607ae0cecb0",
+    "url": "/crypto-dash/static/js/2.ce2ed4f7.chunk.js"
+  },
+  {
+    "revision": "396656086d233a6f84f93b6161ecfda3",
+    "url": "/crypto-dash/static/js/2.ce2ed4f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e9233214477ae1aa61f",
+    "url": "/crypto-dash/static/js/main.75b3670f.chunk.js"
+  },
+  {
+    "revision": "0dc7ab755d494fd4f117",
+    "url": "/crypto-dash/static/js/runtime-main.ae640967.js"
+  }
+]);
